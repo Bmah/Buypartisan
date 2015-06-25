8963c596e654b48984bf6b3eed8fdc84065644e3
Brian M <bkmah@ucdavis.edu>
2015-06-25 15:37:17 -0700

    Added in game sounds and music to the game
    Also added a global variable to control the sfx in the game.

--- Buypartisan/Assets/Scripts/SFXController.cs
+++ Buypartisan/Assets/Scripts/SFXController.cs
@@ -21,7 +21,7 @@ public class SFXController : MonoBehaviour {
 	/// </summary>
 	/// <param name="clipNumber">Clip number.</param>
 	/// <param name="sourceNumber">Source number.</param>
-	void PlayAudioClip(int clipNumber, int sourceNumber){
+	public void PlayAudioClip(int clipNumber, int sourceNumber){
 		if (clipNumber >= SFXList.Length) {
 			Debug.LogError ("Attempted to access SFX clip " + clipNumber + " outside SFXList's range");
 		} else if (sourceNumber >= AudioChannels.Length) {
@@ -38,7 +38,7 @@ public class SFXController : MonoBehaviour {
 	/// <param name="clipNumber">Clip number.</param>
 	/// <param name="sourceNumber">Source number.</param>
 	/// <param name="volume">volume.</param>
-	void PlayAudioClip(int clipNumber, int sourceNumber, float volume){
+	public void PlayAudioClip(int clipNumber, int sourceNumber, float volume){
 		if (clipNumber >= SFXList.Length) {
 			Debug.LogError ("Attempted to access SFX clip " + clipNumber + " outside SFXList's range");
 		} else if (sourceNumber >= AudioChannels.Length) {
