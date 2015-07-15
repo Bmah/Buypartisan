aa6861b6cd75d54a0b9d6a05348a66da918c324e
Chris Ng <thecomediclife324@gmail.com>
2015-07-15 16:19:18 -0700

    New Models

---
+++ Buypartisan/Assets/Scripts/VoterAnimator.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VoterAnimator : MonoBehaviour {
+
+	public void RandomizeIdle() {
+		float saved = Mathf.Round (Random.Range (0f, 6f));
+		GetComponent<Animator> ().SetFloat ("IdleFloat", saved);
+	}
+}
